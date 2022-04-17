409425058ac244abce9fa7a7f6b8162ae4652bed
minionsrahat <rahatuddin786@gmail.com>
2022-04-17 14:09:36 +0600

    Banner Section Designed

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+
+            <section class="section home-hero wf-section">
+                <div class="container-default w-container">
+                    <div class="home-hero-wrapper">
+                        <div
+                            data-w-id="29cd3f96-03a6-481d-9037-14cbd2879b0e"
+                           
+                            class="split-content home-hero-left"
+                        >
+                            <div class="subtitle">Dr. Matthew Anderson</div>
+                            <h1 class="title home-hero">A dedicated doctor you can trust</h1>
+                            <p class="paragraph home-hero">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Elementum eget vel, nunc nulla feugiat. Metus ut ultricies
+                                faucibus.
+                            </p>
+                            <a href="/plans" class="button-primary bg-secondary-1 w-button"
+                            >Book an Appointment</a
+                            >
+                        </div>
+                        <div class="split-content home-hero-right">
+                            <div
+                                data-w-id="e20d7af4-1d3a-1f2b-a0af-9800897e28f4"
+                                class="image-wrapper home-hero"
+                            >
+                                <img
+                                    src="https://assets.website-files.com/601819adb4e3273e5e9cb4e0/601862fd6baef26ae8f5fa5f_image-home-hero-doctor-template.jpg"
+                                    srcset="
+                    https://assets.website-files.com/601819adb4e3273e5e9cb4e0/601862fd6baef26ae8f5fa5f_image-home-hero-doctor-template-p-500.jpeg   500w,
+                    https://assets.website-files.com/601819adb4e3273e5e9cb4e0/601862fd6baef26ae8f5fa5f_image-home-hero-doctor-template-p-800.jpeg   800w,
+                    https://assets.website-files.com/601819adb4e3273e5e9cb4e0/601862fd6baef26ae8f5fa5f_image-home-hero-doctor-template-p-1080.jpeg 1080w,
+                    https://assets.website-files.com/601819adb4e3273e5e9cb4e0/601862fd6baef26ae8f5fa5f_image-home-hero-doctor-template.jpg         1190w
+                  "
+                                    sizes="(max-width: 479px) 93vw, (max-width: 767px) 90vw, (max-width: 991px) 595px, (max-width: 1919px) 47vw, 656.59375px"
+                                    alt="Doctor John Carter - Doctor Webflow Template"
+                                    class="image home-hero"
+                                />
+                                <div
+                                    class="image-solid-cover"
+                                ></div>
+                            </div>
+                            <img
+                                src="https://assets.website-files.com/601819adb4e3273e5e9cb4e0/6018589e3ed02ef5bdb1fbe4_bg-home-hero-doctor-template.svg"
+                                data-w-id="f5323537-1a51-9965-7156-513075d69756"
+                                              alt=""
+                                class="bg home-hero-right"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="bg home-hero"></div>
+            </section>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header w-nav"
+            data-collapse="medium"
+            data-animation="over-right"
+            data-duration="400"
+            data-w-id="58db7844-5919-d71b-dd74-2323ed8dffe9"
+            data-easing="ease"
+            data-easing2="ease"
+            role="banner">
+            <div className="container-default w-container">
+                <div className="header-contact-wrapper">
+                    <a href="tel:(284)587-7800" className="header-contact-link"
+                    >(284) 587 - 7800</a
+                    ><a
+                        href="mailto:contact@doctor.com"
+                        className="header-contact-link last"
+                    >contact@doctor.com</a
+                    >
+                </div>
+                <div className="divider header-divider"></div>
+                <div className="header-wrapper">
+                    <div className="split-content header-right">
+                        <a
+                            href="/"
+                            className="brand w-nav-brand w--current"
+                            aria-label="home"
+                        >
+                            <img
+                                alt=''
+                                src="https://assets.website-files.com/601819adb4e3273e5e9cb4e0/601839a525b0824fdb1566b0_logo-doctor-template.svg"
+                                className="header-logo"
+                            />
+                        </a>
+                        <nav role="navigation" className="nav-menu w-nav-menu">
+                            <ul className="header-navigation">
+                                <li className="nav-item-wrapper">
+                                    <a href="/" aria-current="page" className="nav-link w--current"
+                                    >Home</a
+                                    >
+                                </li>
+                             
+                                <li className="nav-item-wrapper">
+                                    <a href="/about" className="nav-link">About</a>
+                                </li>
+                                <li className="nav-item-wrapper">
+                                    <a href="/services" className="nav-link">Services</a>
+                                </li>
+                                <li className="nav-item-wrapper">
+                                    <a href="/contact" className="nav-link">Contact</a>
+                                </li>
+                                <li className="nav-item-wrapper header-button-mobile">
+                                    <a
+                                        href="/plans"
+                                        className="button-primary bg-secondary-1 header-button-mobile w-button"
+                                    >Book Now</a
+                                    >
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="split-content header-left">
+                     
+                        <a
+                            href="/plans"
+                            className="button-primary bg-secondary-1 header-button w-button"
+                        >Book Now</a
+                        >
+                        <div
+                            className="menu-button w-nav-button"
+                            
+                            aria-label="menu"
+                            role="button"
+                            tabIndex="0"
+                            aria-controls="w-nav-overlay-0"
+                            aria-haspopup="menu"
+                            aria-expanded="false"
+                        >
+                            <div className="menu-button-icon-wrapper">
+                                <div className="menu-button-icon">
+                                    <div className="menu-line-top"></div>
+                                    <div className="menu-line-middle"></div>
+                                    <div className="menu-line-bottom"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0"></div>
+        </div>
+    );
+};
+
+export default Header;
